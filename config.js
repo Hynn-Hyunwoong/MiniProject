9a1b8a0cc71d332bfbea9ab2029746890b259b86
Hynn <choihwoong@me.com>
2023-01-13 06:52:18 +0900

    change to config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const NodeENV = process.env.NODE_ENV || 'develope';
-const host = process.env.HOST || '127.0.0.0';
-const port = process.env.PORT || '3010';
-const DBHost = process.env.DB_HOST || '123';
-const DBPort = process.env.DB_PORT || '456';
-const DBUser = process.env.DB_USER || 'Google';
-const DBPw = process.env.DB_PASSWORD || 'Apple';
-const DBDb = process.env.DB_DATABASE || 'Hello';
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || '3000';
+const DBHost = process.env.DB_HOST || '127.0.0.1';
+const DBPort = process.env.DB_PORT || '3306';
+const DBUser = process.env.DB_USER || 'root';
+const DBPw = process.env.DB_PASSWORD || 'Apple1984!';
+const DBDb = process.env.DB_DATABASE || 'Miniproject';
 
 
 const config = {
